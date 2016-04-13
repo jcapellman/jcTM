9d6289504779c134742b1399563c459d0f0ea160
jcapellman <jcapellman@hotmail.com>
2016-04-12 22:06:03 -0400

    Added Low and High Temp population in the TemperatureController

--- csharp/jcTM.WebAPI/Controllers/TemperatureController.cs
+++ csharp/jcTM.WebAPI/Controllers/TemperatureController.cs
@@ -8,8 +8,7 @@ using jcTM.WebAPI.DataLayer.Entities;
 namespace jcTM.WebAPI.Controllers {
     [System.Web.Mvc.RoutePrefix("api/Temperature")]
     public class TemperatureController : ApiController {
-        public void GET(double temperature)
-        {
+        public void GET(double temperature) {
             using (var eFactory = new jctmEntities()) {
                 var temp = eFactory.Temperatures.Create();
 
@@ -23,18 +22,19 @@ namespace jcTM.WebAPI.Controllers {
             }
         }
 
-        public LatestTemperatureResponseItem GET()
-        {
+        public DashboardResponseItem GET() {
             using (var eFactory = new jctmEntities()) {
                 var result = eFactory.WEBAPI_getLatestTemperatureSP().FirstOrDefault();
 
                 if (result == null) {
-                    return new LatestTemperatureResponseItem();
+                    return new DashboardResponseItem();
                 }
 
-                return new LatestTemperatureResponseItem {
-                    RecordedTime = result.Modified,
-                    Temperature = result.Temperature
+                return new DashboardResponseItem {
+                    Latest_RecordedTime = result.Modified,
+                    Latest_Temperature = result.Temperature,
+                    CurrentDay_HighTemperature = result.Max.HasValue ? Math.Round(result.Max.Value, 0) : 0.0,
+                    CurrentDay_LowTemperature = result.Min.HasValue ? Math.Round(result.Min.Value, 0) : 0.0,
                 };
             }
         }
