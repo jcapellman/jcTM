32a78b0c13dfd1ad7cb185d508739f75bb33f441
jcapellman <jcapellman@hotmail.com>
2016-04-12 22:05:18 -0400

    Added Min and Max Temperatures to the DashboardResponseItem Class

---
+++ csharp/jcTM.PCL/Transports/DashboardResponseItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace jcTM.PCL.Transports {
+    [DataContract]
+    public class DashboardResponseItem {
+        [DataMember]
+        public double Latest_Temperature { get; set; }
+
+        [DataMember]
+        public DateTime Latest_RecordedTime { get; set; }
+
+        [DataMember]
+        public double CurrentDay_HighTemperature { get; set; }
+
+        [DataMember]
+        public double CurrentDay_LowTemperature { get; set; }
+    }
+}
