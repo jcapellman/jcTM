fef3bf772bcd7e9ff5fcb86ba1ac3f32d418d2df
jcapellman <jcapellman@hotmail.com>
2016-04-17 12:18:03 -0400

    Added: NightlyJob Support for Azure Schedule Jobs

---
+++ csharp/jcTM.WebAPI/Controllers/NightlyJobController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+using jcTM.WebAPI.DataLayer.Entities;
+
+namespace jcTM.WebAPI.Controllers {
+    [System.Web.Mvc.RoutePrefix("api/NightlyJob")]
+    public class NightlyJobController : ApiController {
+        public void GET() {
+            using (var eFactory = new jctmEntities()) {
+                eFactory.Database.ExecuteSqlCommand("EXEC dbo.SQL_runNightlyJobsSP");
+            }
+        }
+    }
+}
