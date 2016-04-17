05625d974bea121134d91221fcd56efbbda9ddbf
jcapellman <jcapellman@hotmail.com>
2016-04-16 21:09:35 -0400

    Finished creating wrappers for WebAPI calls in the UWP App

--- csharp/jcTM.PCL/Handlers/TemperatureHandler.cs
+++ csharp/jcTM.PCL/Handlers/TemperatureHandler.cs
@@ -1,5 +1,18 @@
-﻿namespace jcTM.PCL.Handlers {
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using jcTM.PCL.Transports;
+
+namespace jcTM.PCL.Handlers {
     public class TemperatureHandler : BaseHandler {        
         public void RecordTemperature(double temperature) { GET($"Temperature?temperature={temperature}"); }
+
+        public async Task<DashboardResponseItem> GetDashboard() => await GET<DashboardResponseItem>("Temperature");
+
+        public async Task<List<DayOverviewListingResponseItem>> GetDayOverviewListing() => await GET<List<DayOverviewListingResponseItem>>("DayOverviewReport");
+
+        public async Task<List<DayOverviewDetailResponseItem>> GetDayOverviewGraph(string dateTime) => 
+            await GET<List<DayOverviewDetailResponseItem>>($"DayOverviewReport?selectedDay={dateTime}");
     }
 }
